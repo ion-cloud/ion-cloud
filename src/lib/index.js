9b6b0b0bd76dde71b6722a6dbdef864fb2a6fbcd
Nathaniel Inman <nate041010@gmail.com>
2016-12-21 20:24:12 -0600

    linked phaser to ion-cloud.

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,4 @@ export {Ion} from './ion';
 export {IonCloud} from './ionCloud';
 export {easel,Easel} from './easel';
 export {ink,rgb2hex,randomHex,randomRgb} from './ink';
-
+export {Phaser} from './phaser';
