390223496fa0f6fffd722d25d978ccc92de32fd6
Nathaniel Inman <nate041010@gmail.com>
2017-01-05 08:37:46 -0600

    exporting functions hex2rgbObject and hex2rgb.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 export {Ion} from './ion';
 export {IonCloud} from './ionCloud';
 export {easel,Easel} from './easel';
-export {ink,rgb2hex,randomHex,randomRgb} from './ink';
+export {ink,rgb2hex,randomHex,randomRgb,hex2rgb,hex2rgbObject} from './ink';
 export {Phaser} from './phaser';
